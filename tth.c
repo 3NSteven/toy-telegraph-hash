3da83435ff1f201d896b5ef0a7108e0226dd586f
3NSteven <pereira.steven36@gmail.com>
2023-03-17 15:37:13 +0100

    Calcul complet de l'empreinte

--- tth.c
+++ tth.c
@@ -5,6 +5,7 @@
 #define EMPREINTE 5
 #define VAL_PAR_BLOC (EMPREINTE*EMPREINTE)
 #define VAL_PAR_CASE 64
+#define VAL_PADDING 32
 
 void encodage_msg(char* chaine, int* new_chaine){
     for(int i = 0; i < strlen(chaine); i++){
@@ -19,7 +20,7 @@ int padding(int* chaine, int length){
         int is_first_pad = 1;
         for(int i = length; i < (topad+length); i++){
             if(is_first_pad == 1){
-                chaine[i] = 32;
+                chaine[i] = VAL_PADDING;
                 is_first_pad = 0;
             }
             else{
@@ -50,8 +51,8 @@ int main(int argc, char* argv[]){
 
     encodage_msg(argv[1], chaine);
     
-
-
+/*
+    //chaine exemple
     chaine[0] = 0;
     chaine[1] = 6;
     chaine[2] = 8;
@@ -89,9 +90,9 @@ int main(int argc, char* argv[]){
     chaine[34] = 17;
     chaine[35] = 34;
     chaine[36] = 1;
+*/
 
-
-
+    //print
     printf("encodage:\n");
     for(int i = 0; i < strlen(argv[1]); i++){
         printf("%d ", chaine[i]);
@@ -100,6 +101,7 @@ int main(int argc, char* argv[]){
 
     int message_length = padding(chaine, strlen(argv[1]));
 
+    //print
     printf("padding:\n");
     for(int i = 0; i < message_length; i++){
         printf("%d ", chaine[i]);
@@ -117,6 +119,7 @@ int main(int argc, char* argv[]){
         }
     }
 
+    //print
     printf("matrices:\n");
     for(int i = 0; i < nb_blocs; i++){ //pour chaque bloc
         for(int j = 0; j < VAL_PAR_BLOC; j++){  //pour chaque element du bloc
@@ -126,107 +129,94 @@ int main(int argc, char* argv[]){
         printf("\n\n");
     }
 
-    int empreintes[nb_blocs][EMPREINTE];
-    printf("empreintes 1:\n");
-    for(int i = 0; i < nb_blocs; i++){  //pour chaque bloc
-        for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
-            empreintes[i][j] = 0;
-        }
-        printf("\n\n");
+    //initialisation de l'empreinte
+    int empreinte[EMPREINTE];
+    for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
+        empreinte[j] = 0;
     }
 
-
-    //calcul empreinte etape 1  (SOMME ET MODULO)
-    //int empreintes[nb_blocs][EMPREINTE];
+    //CALCUL EMPREINTE
     for(int i = 0; i < nb_blocs; i++){  //pour chaque bloc
+        printf("CALCUL EMPREINTE BLOC n.%d\n", i);
+        //calcul empreinte etape 1  (SOMME ET MODULO)
         for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
             for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la colonne (somme)
-                empreintes[i][j] += matrices[i][k][j];
+                empreinte[j] += matrices[i][k][j];
             }
-            //printf("%d ", empreintes[i][j]);
-            empreintes[i][j] = empreintes[i][j]%VAL_PAR_CASE;
+            empreinte[j] = empreinte[j]%VAL_PAR_CASE;
         }
-        //printf("\n\n");
-    }
 
-    printf("empreintes etape 1:\n");
-    for(int i = 0; i < nb_blocs; i++){  //pour chaque bloc
+        //print
+        printf("empreinte etape 1:\n");
         for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
-            printf("%d ", empreintes[i][j]);
+            printf("%d ", empreinte[j]);
         }
         printf("\n\n");
+
+        //calcul empreinte etape 2 (DECALAGE)
+        int new_matrice[EMPREINTE][EMPREINTE];
+        int decalage, ajouter;
+        int retenues[EMPREINTE];
+        for(int j = 0; j < EMPREINTE; j++){ //pour chaque ligne //0
+            decalage = j;
+            ajouter = 0;
+            for(int l = 0; l < EMPREINTE; l++){
+                retenues[l] = 0;
+            }
+            for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la ligne (decalage)   //1
+                if(j == 0){
+                    new_matrice[j][k] = matrices[i][j][k];
+                }
+                else if((EMPREINTE-k) <= decalage){
+                    new_matrice[j][ajouter] = matrices[i][j][k];
+                    ajouter++;
+                    //new_matrice[j][k] = matrices[i][j][ajouter];
+                }
+            }
+            if(j != 0){
+                while(ajouter < EMPREINTE){
+                    new_matrice[j][ajouter] = matrices[i][j][ajouter-j];
+                    ajouter++;
+                }
+            }
+        }
+
+        printf("matrice etape 2:\n");
+        for(int j = 0; j < VAL_PAR_BLOC; j++){  //pour chaque element du bloc
+            if(j%EMPREINTE == 0) printf("\n");
+            printf("%d ", new_matrice[j/EMPREINTE%EMPREINTE][j%EMPREINTE]);
+        }
+        printf("\n\n");
+
+        //calcul empreinte etape 3 (SOMME ET MODULO)
+        for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
+            for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la colonne (somme)
+                empreinte[j] += new_matrice[k][j];
+            }
+            //printf("%d ", empreinte[i][j]);
+            empreinte[j] = empreinte[j]%VAL_PAR_CASE;
+        }
+        //printf("\n\n");
+
+        printf("empreinte etape 3:\n");
+        for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
+            printf("%d ", empreinte[j]);
+        }
+        printf("\n\n");
+        
     }
 
-    
-    //calcul empreinte etape 2 (DECALAGE)
-    int i = 0;  //bloc
-    int new_matrice[EMPREINTE][EMPREINTE];
-    int decalage, ajouter;
-    int retenues[EMPREINTE];
-    for(int j = 0; j < EMPREINTE; j++){ //pour chaque ligne //0
-        decalage = j;
-        ajouter = 0;
-        for(int l = 0; l < EMPREINTE; l++){
-            retenues[l] = 0;
-        }
-        for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la ligne (decalage)   //1
-            if(j == 0){
-                new_matrice[j][k] = matrices[i][j][k];
-            }
-            else if((EMPREINTE-k) <= decalage){
-                new_matrice[j][ajouter] = matrices[i][j][k];
-                ajouter++;
-                //new_matrice[j][k] = matrices[i][j][ajouter];
-            }
-        }
-        if(j != 0){
-            while(ajouter < EMPREINTE){
-                new_matrice[j][ajouter] = matrices[i][j][ajouter-j];
-                ajouter++;
-            }
-        }
-    }
-    
-
-    /*
-    void * source;
-    void * destination;
-    size_t size;
-    int new_matrice[EMPREINTE][EMPREINTE];
-    int i = 0;  //bloc
-    for(int j = 0; j < EMPREINTE; j++){ //pour chaque ligne
-        for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la ligne (decalage)
-            source = (void *) matrices[i][j];
-            destination = (void *) ( new_matrice[j] + 1 );
-            size = EMPREINTE * sizeof( int );
-            memmove( destination, source, size );
-        }
+    printf("##################################################################\n\n");
+    printf("TTH^%dv%d(\"%s\") = ", EMPREINTE, VAL_PAR_CASE, argv[1]);
+    for(int i = 0; i < EMPREINTE; i++){ //pour chaque colonne
+        printf("[%d]", empreinte[i]);
     }
     printf("\n\n");
-    */
+    printf("##################################################################\n\n");
 
-    printf("matrice etape 2:\n");
-    for(int j = 0; j < VAL_PAR_BLOC; j++){  //pour chaque element du bloc
-        if(j%EMPREINTE == 0) printf("\n");
-        printf("%d ", new_matrice[j/EMPREINTE%EMPREINTE][j%EMPREINTE]);
-    }
-    printf("\n\n");
-
-    //calcul empreinte etape 3 (SOMME ET MODULO)
-    i = 0;  //bloc
-    for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
-        for(int k = 0; k < EMPREINTE; k++){ //pour chaque case de la colonne (somme)
-            empreintes[i][j] += new_matrice[k][j];
-        }
-        //printf("%d ", empreintes[i][j]);
-        empreintes[i][j] = empreintes[i][j]%VAL_PAR_CASE;
-    }
-    //printf("\n\n");
-
-    printf("empreintes etape 3:\n");
-    i = 0;  //bloc
-    for(int j = 0; j < EMPREINTE; j++){ //pour chaque colonne
-        printf("%d ", empreintes[i][j]);
+    printf("L'empreinte du message: \n\n%s\n \navec l'algorithme Toy-Hash-Telegraph^%dv%d (travaillant sur des valeurs numérique modulo %d et dont l'empreinte résultante est constituée de %d valeurs) =\n\n", argv[1], EMPREINTE, VAL_PAR_CASE, VAL_PAR_CASE, EMPREINTE);
+    for(int i = 0; i < EMPREINTE; i++){ //pour chaque colonne
+        printf("[%d]", empreinte[i]);
     }
     printf("\n\n");
 
